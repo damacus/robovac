e5e0027f97e2e97bae9e50ba13fe264257c5d844
Dan Webb <dan.webb@damacus.io>
2025-05-06 22:47:23 +0100

    Tidy model list
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- custom_components/robovac/vacuums/__init__.py
+++ custom_components/robovac/vacuums/__init__.py
@@ -10,7 +10,6 @@ from .T2130 import T2130
 from .T2132 import T2132
 from .T2150 import T2150
 from .T2181 import T2181
-from .T2182 import T2182
 from .T2190 import T2190
 from .T2192 import T2192
 from .T2193 import T2193
@@ -21,14 +20,8 @@ from .T2252 import T2252
 from .T2253 import T2253
 from .T2254 import T2254
 from .T2255 import T2255
-from .T2256 import T2256
-from .T2257 import T2257
-from .T2258 import T2258
-from .T2259 import T2259
 from .T2261 import T2261
 from .T2262 import T2262
-from .T2266 import T2266
-from .T2267 import T2267
 from .T2270 import T2270
 from .T2272 import T2272
 from .T2273 import T2273
@@ -37,7 +30,6 @@ from .T2276 import T2276
 from .T2277 import T2277
 from .T2278 import T2278
 from .T2320 import T2320
-from .T2351 import T2351
 
 ROBOVAC_MODELS = {
     "T2103": T2103,
@@ -57,27 +49,19 @@ ROBOVAC_MODELS = {
     "T2254": T2254,
     "T2150": T2150,
     "T2255": T2255,
-    "T2256": T2256,
-    "T2257": T2257,
-    "T2258": T2258,
-    "T2259": T2259,
     "T2270": T2270,
     "T2272": T2272,
     "T2273": T2273,
     "T2181": T2181,
-    "T2182": T2182,
     "T2190": T2190,
     "T2192": T2192,
     "T2193": T2193,
     "T2194": T2194,
-    "T2267": T2267,
     "T2277": T2277,
     "T2278": T2278,
     "T2261": T2261,
     "T2262": T2262,
-    "T2266": T2266,
     "T2320": T2320,
-    "T2351": T2351,
     "T2275": T2275,
     "T2276": T2276,
 }
