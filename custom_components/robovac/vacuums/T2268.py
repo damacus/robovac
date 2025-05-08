3621000e61c6095d304dbe7e8117b085e8253ba7
MartinBachmannHD <98031123+MartinBachmannHD@users.noreply.github.com>
2025-05-08 20:53:03 +0200

    Adding RobovacModelDetails
    Signed-off-by: MartinBachmannHD <98031123+MartinBachmannHD@users.noreply.github.com>

--- custom_components/robovac/vacuums/T2268.py
+++ custom_components/robovac/vacuums/T2268.py
@@ -1,8 +1,8 @@
 from homeassistant.components.vacuum import VacuumEntityFeature
-from .base import RoboVacEntityFeature, RobovacCommand
+from .base import RoboVacEntityFeature, RobovacCommand, RobovacModelDetails
 
 
-class T2268:
+class T2268(RobovacModelDetails):
     homeassistant_features = (
         VacuumEntityFeature.BATTERY
 #        | VacuumEntityFeature.CLEAN_SPOT
